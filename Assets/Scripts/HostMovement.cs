69f74a973434fcb0096e85a15901458fd353694d
alendugalic <137260328+alendugalic@users.noreply.github.com>
2023-11-17 20:04:55 +1100

    the start of something

--- Assets/Scripts/HostMovement.cs
+++ Assets/Scripts/HostMovement.cs
@@ -1,12 +1,43 @@
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HostMovement : NetworkBehaviour
 {
+    private Rigidbody hostRb;
+    private PlayerInput playerInput;
+
+    [Header("HOST PRIMARY STATS")]
+
+    [SerializeField]
+    [Tooltip("On ground Movespeed")]
+    [Range(0f, 100f)]
     public float movementSpeed = 5f;
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    [Tooltip("Normal jump power (heavy jump is X2")]
+    [Range(0f, 100f)]
+    private float jumpPower = 5f;
+
+    [SerializeField]
+    [Tooltip("Host attack power")]
+    [Range(0f, 100f)]
+    private float attackPower = 5f;
+
+    [SerializeField]
+    [Tooltip("Heavy attack cooldown")]
+    [Range(0f, 100f)]
+    private float heavyAttackCooldown = 5f;
+
+    [SerializeField]
+    [Tooltip("Super jump cooldown")]
+    [Range(0f, 100f)]
+    private float superJumpCooldown = 5f;
+
+    private void Awake()
     {
+        hostRb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
         
     }
 
@@ -14,18 +45,89 @@ public class HostMovement : NetworkBehaviour
     void Update()
     {
         if (!IsOwner) return;
-       
+
+
+
     }
 
     private void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // Calculate movement direction
-        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized;
-
-        // Move the player
-        transform.Translate(movement * movementSpeed * Time.deltaTime);
+        
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+            Debug.Log("I moved " + context.phase);
+            Vector2 inputVector = context.ReadValue<Vector2>();
+            hostRb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * movementSpeed, ForceMode.Force);
+       
+    }
+    public void Sprint(InputAction.CallbackContext context)
+    {
+
+    }
+   //The controls for the Host facing direction
+    public void Facing(InputAction.CallbackContext context)
+    {
+
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            hostRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            Debug.Log("I jumped " + context.phase);
+        }
+       
+    }
+    public void Attack(InputAction.CallbackContext context)
+    {
+
+    }
+    public void HeavyAttack(InputAction.CallbackContext context) 
+    {
+        
+    }
+    public void SuperJump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            hostRb.AddForce(Vector3.up * jumpPower * 4, ForceMode.Impulse);
+        }
+    }
+    public void Block(InputAction.CallbackContext context)
+    {
+
+    }
+    public void AreaBlock(InputAction.CallbackContext context)
+    {
+
+    }
+    public void TargetLock(InputAction.CallbackContext context)
+    {
+
+    }
+    public void Eat(InputAction.CallbackContext context)
+    {
+
+    }
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (playerInput.currentActionMap.name == "Player-Host")
+            {
+                playerInput.SwitchCurrentActionMap("UI");
+            }
+            else if (playerInput.currentActionMap.name == "UI")
+            {
+                playerInput.SwitchCurrentActionMap("Player-Host");
+            }
+            Debug.Log("Paused " + context.phase);
+        }
+
+        
+    }
+
 }
